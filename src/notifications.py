ceddf783167c591d7d0c4ad5846ddc83f5791987
DanikoHub <194267398+DanikoHub@users.noreply.github.com>
2025-01-28 11:53:07 +0000

    Refactored code

--- src/notifications.py
+++ src/notifications.py
@@ -2,17 +2,20 @@ import re
 import json
 
 with open('./mysite/secret_var.json', 'r') as file:
-    secret_var = json.load(file)
+	secret_var = json.load(file)
 
 from mysite.src.tables.users import Users
 from mysite.src.tables.issues import Issues
 from mysite.src.tables.comment_branch import CommentBranch
 
-from mysite.src.sql_requests import select_by_field, select_all_from_list
+from mysite.src.sql_requests import select_by_field, select_all_from_list, update_obj
+
+def send_to_users(bot, users_to_send, text):
+	send = lambda user, text : bot.send_message(user, text)
+	[send(u, text) for u in users_to_send]
 
 def new_comment(bot, request, users_to_send):
-    for u in users_to_send:
-        bot.send_message(u, "Новый комментарий в - " + request.json["object_attributes"]["url"])
+	send_to_users(bot, users_to_send, "Новый комментарий в - " + request.json["object_attributes"]["url"])
 
 def issue_change(Session, bot, request, users_to_send):
 	if request.json["event_type"] == 'issue':
@@ -20,99 +23,84 @@ def issue_change(Session, bot, request, users_to_send):
 		issue = select_by_field(Session, Issues, Issues.issueId, int(request.json["object_attributes"]["id"]))
 
 		if 'id' in request.json["changes"].keys():
-			for u in users_to_send:
-				bot.send_message(u, "Новая issue - " + request.json["object_attributes"]["url"])
+			send_to_users(bot, users_to_send, "Новая issue - " + request.json["object_attributes"]["url"])
 
 		elif 'description' in request.json["changes"].keys():
-			for u in users_to_send:
-				bot.send_message(u, "Изменено описание в issue - " + request.json["object_attributes"]["url"])
+			send_to_users(bot, users_to_send, "Изменено описание в issue - " + request.json["object_attributes"]["url"])
 
 		elif 'assignees' in request.json["changes"].keys():
-			for u in users_to_send:
-				bot.send_message(u, "Изменены ответственные в issue - " + request.json["object_attributes"]["url"])
+			send_to_users(bot, users_to_send, "Изменены ответственные в issue - " + request.json["object_attributes"]["url"])
 
 		elif 'state_id' in request.json["changes"].keys():
-			if issue[0].isClosed != int(request.json["changes"]["state_id"]["current"]):
+			try:
+				update_obj(Session, Issues, Issues.issueId, int(request.json["object_attributes"]["id"]), {'isClosed' : int(request.json["changes"]["state_id"]["current"])}, bot)
 
-				with Session() as session:
-					try:
-						issue_to_update = session.query(Issues).filter(Issues.issueId == int(request.json["object_attributes"]["id"]))
-						issue_to_update.update({'isClosed' : int(request.json["changes"]["state_id"]["current"])})
-					except Exception as e:
-						session.rollback()
-						if bot is not None:
-							bot.send_message(secret_var["telegram_id"], 'not43 ' + str(e))
-					else:
-						session.commit()
+				if issue[0].isClosed != int(request.json["changes"]["state_id"]["current"]):
+					send_to_users(bot, users_to_send, "Issue была переоткрыта - " + request.json["object_attributes"]["url"] \
+					if request.json["changes"]["state_id"]["current"] == 1 \
+					else "Issue была закрыта - " + request.json["object_attributes"]["url"])
 
-				if request.json["changes"]["state_id"]["current"] == 1:
-					for u in users_to_send:
-						bot.send_message(u, "Issue была переоткрыта - " + request.json["object_attributes"]["url"])
+			except Exception as e:
+				if bot is not None:
+					bot.send_message(secret_var["telegram_id"], 'not44 ' + str(e))
 
-				if request.json["changes"]["state_id"]["current"] == 2:
-					for u in users_to_send:
-						bot.send_message(u, "Issue была закрыта - " + request.json["object_attributes"]["url"])
 
 def labels_change(bot, request, users_to_send):
-    if 'labels' in request.json["changes"].keys() and users_to_send is not None:
-        lbl_list = ''
+	if 'labels' in request.json["changes"].keys() and users_to_send is not None:
+		lbl_list = ''
 
-        for lbl in request.json["changes"]["labels"]["current"]:
-            lbl_list += lbl["title"] + ", "
+		for lbl in request.json["changes"]["labels"]["current"]:
+			lbl_list += lbl["title"] + ", "
 
-        lbl_list = lbl_list[:-2]
-        for u in users_to_send:
-            bot.send_message(u,
-            "Были изменены лейблы в issue - " +
-            request.json["object_attributes"]["url"] +
-            "\nАкутальные лейблы - " + lbl_list
-            )
+		lbl_list = lbl_list[:-2]
+		send_to_users(bot, users_to_send, "Были изменены лейблы в issue - " + request.json["object_attributes"]["url"] + "\nАкутальные лейблы - " + lbl_list)
 
 def get_users_for_notification(Session, request, bot = None):
-    users_to_send = set()
-    initiator_giltab_id = int(request.json["user"]["id"])
+	users_to_send = set()
+	initiator_giltab_id = int(request.json["user"]["id"])
 
-    if request.json["event_type"] == 'issue':
-        author = select_by_field(Session, Users, Users.gitlabId, int(request.json["object_attributes"]["author_id"]))
+	if request.json["event_type"] == 'issue':
+		author = select_by_field(Session, Users, Users.gitlabId, int(request.json["object_attributes"]["author_id"]))
 
-        if author is not None:
-            users_to_send.add(author[0].telegramId)
+		if author is not None:
+			users_to_send.add(author[0].telegramId)
 
-        if request.json["object_attributes"]["assignee_ids"] is not None:
+		if request.json["object_attributes"]["assignee_ids"] is None:
+			return users_to_send
 
-            int_assignee_ids = [int(i) for i in request.json["object_attributes"]["assignee_ids"]]
+		int_assignee_ids = [int(i) for i in request.json["object_attributes"]["assignee_ids"]]
 
-            if len(int_assignee_ids) > 0:
-                assignees = select_all_from_list(Session, Users, Users.gitlabId, int_assignee_ids, Users.telegramId)
+		if len(int_assignee_ids) > 0:
+			assignees = select_all_from_list(Session, Users, Users.gitlabId, int_assignee_ids, Users.telegramId)
 
-                if assignees is not None:
-                    users_to_send.update(assignees)
+			if assignees is not None:
+				users_to_send.update(assignees)
 
 
 
-    if request.json["event_type"] == 'note':
-        try:
-            comment = request.json["object_attributes"]["description"]
-            mentions = re.findall(r"@\w+", comment)
+	if request.json["event_type"] == 'note':
+		try:
+			comment = request.json["object_attributes"]["description"]
+			mentions = re.findall(r"@\w+", comment)
 
-            mentioned = select_all_from_list(Session, Users, class_field = Users.gitlabUsername, list_vals = mentions, return_field = Users.telegramId)
-            if mentioned is not None:
-                users_to_send.update(mentioned)
+			mentioned = select_all_from_list(Session, Users, class_field = Users.gitlabUsername, list_vals = mentions, return_field = Users.telegramId)
+			if mentioned is not None:
+				users_to_send.update(mentioned)
 
-            branche_comments = select_by_field(Session, CommentBranch, CommentBranch.discussionId, request.json["object_attributes"]["discussion_id"])
+			branche_comments = select_by_field(Session, CommentBranch, CommentBranch.discussionId, request.json["object_attributes"]["discussion_id"])
 
-            if branche_comments is None:
-                return users_to_send
+			if branche_comments is None:
+				return users_to_send
 
-            for b in branche_comments:
-                if b.userGitlabId != initiator_giltab_id:
-                    branch_participant = select_by_field(Session, Users, Users.gitlabId, b.userGitlabId)
-                    users_to_send.add(branch_participant[0].telegramId)
+			for b in branche_comments:
+				if b.userGitlabId != initiator_giltab_id:
+					branch_participant = select_by_field(Session, Users, Users.gitlabId, b.userGitlabId)
+					users_to_send.add(branch_participant[0].telegramId)
 
-        except Exception as e:
-            if bot is not None:
-                bot.send_message(secret_var["telegram_id"], 'not89 ' + str(e))
+		except Exception as e:
+			if bot is not None:
+				bot.send_message(secret_var["telegram_id"], 'not89 ' + str(e))
 
-    return users_to_send
+	return users_to_send
 
 
--- src/sql_requests.py
+++ src/sql_requests.py
@@ -2,98 +2,94 @@ from sqlalchemy import select
 import json
 
 with open('./mysite/secret_var.json', 'r') as file:
-    secret_var = json.load(file)
+	secret_var = json.load(file)
+
+
+def send_e(bot, e, line = ''):
+	if bot is not None:
+		bot.send_message(secret_var["telegram_id"], line + str(e))
+
 
 def create_obj(Session, composed_obj, class_name = None, filter_ = None, bot = None):
-    try:
-        if class_name is not None and filter_ is not None:
-            obj_in_db = select_filtered_rows(Session, class_name, filter_, bot = bot)
-        else:
-            obj_in_db = []
-        with Session() as session:
-            try:
-                if obj_in_db is None or obj_in_db == []:
-                    session.add(composed_obj)
-            except:
-                session.rollback()
-            else:
-                session.commit()
-    except Exception as e:
-        if bot is not None:
-            bot.send_message(secret_var["telegram_id"], 'sql22 ' + str(e))
+	try:
+		obj_in_db = select_filtered_rows(Session, class_name, filter_, bot = bot) \
+		if class_name is not None and filter_ is not None \
+		else []
 
-# def update_obj(Session, class_name, update_obj, class_update_field, update_value, bot = None):
-# 	with Session() as session:
-# 		try:
-# 			session.query(class_name).filter(class_field == search_value).update(new_value)
-# 		except Exception as e:
-# 			session.rollback()
-# 			if bot is not None:
-# 			    bot.send_message(secret_var["telegram_id"], 'sql32 ' + str(e))
-# 		else:
-# 			session.commit()
-# 			if bot is not None:
-# 			    bot.send_message(secret_var["telegram_id"], 'commited')
+		with Session() as session:
+			try:
+				if obj_in_db is None or obj_in_db == []:
+					session.add(composed_obj)
+			except:
+				session.rollback()
+			else:
+				session.commit()
+
+	except Exception as e:
+	    send_e(bot, e, line = 'sql29 ')
+
+
+def update_obj(Session, class_name, class_update_field, update_value, update_json, bot = None):
+	with Session() as session:
+		try:
+			obj_to_update = session.query(class_name).filter(class_update_field == update_value)
+			obj_to_update.update(update_json)
+
+		except Exception as e:
+			session.rollback()
+			send_e(bot, e, line = 'sql40 ')
+
+		else:
+			session.commit()
+
 
 def select_all(Session, class_name, return_field = None):
-    if return_field is None:
-        statement = select(class_name)
-    else:
-        statement = select(return_field)
+	statement = select(class_name if return_field is None else return_field)
 
-    with Session() as session:
-        db_object = session.scalars(statement).all()
+	with Session() as session:
+		db_object = session.scalars(statement).all()
 
-    return db_object
+	return db_object
+
 
 def select_by_field(Session, class_name, class_field, value, return_field = None):
-    try:
-        if return_field is None:
-            statement = select(class_name).where(class_field == value)
-        else:
-            statement = select(return_field).where(class_field == value)
+	try:
+		statement = select(class_name if return_field is None else return_field).where(class_field == value)
 
-        with Session() as session:
-            db_object = session.scalars(statement).all()
+		with Session() as session:
+			db_object = session.scalars(statement).all()
 
-        return db_object
-    except:
-        pass
+		return db_object
+
+	except:
+		pass
+
 
 def select_filtered_rows(Session, class_name, filters, return_field = None, bot = None):
 	try:
 		results = None
 
 		with Session() as session:
-			if return_field is None:
-				query = session.query(class_name)
-			else:
-				query = session.query(return_field)
+			query = session.query(class_name if return_field is None else return_field)
 
-			for field, value in filters.items():
-					query = query.filter(getattr(class_name, field) == value)
-
-			results = query.all()
+			results = [query.filter(getattr(class_name, field) == value) for field, value in filters.items()][-1].all()
 
 		return results
 
 	except Exception as e:
-		if bot is not None:
-			bot.send_message(secret_var["telegram_id"], 'sql69 ' + str(e))
+	    send_e(bot, e, line = 'sql80 ')
+
 
 def select_all_from_list(Session, class_name, class_field, list_vals, return_field = None):
-    try:
-        if return_field is None:
-    	    statement = select(class_name).where(class_field.in_(list_vals))
-        else:
-            statement = select(return_field).where(class_field.in_(list_vals))
+	try:
+		statement = select(class_name if return_field is None else return_field).where(class_field.in_(list_vals))
 
-        with Session() as session:
-            db_object = session.scalars(statement).all()
+		with Session() as session:
+			db_object = session.scalars(statement).all()
 
-        return db_object
-    except:
-        pass
+		return db_object
+	except:
+		pass
 
 
 
