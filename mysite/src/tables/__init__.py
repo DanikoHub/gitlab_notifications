ab4a79c277df0be599dc26206b4a10bb4b01e9a4
Даниил Боровков <daniil@MacBook-Air-Daniil.local>
2025-02-09 09:24:04 +0300

    Created module tables

---
+++ mysite/src/tables/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "Users",
+    "Issues",
+    "CommentBranch",
+    "Labels",
+    "LabelsTaskLink",
+]
+
+from .users import Users
+from .issues import Issues
+from .comment_branch import CommentBranch
+from .labels import Labels
+from .labels_task_link import LabelsTaskLink
