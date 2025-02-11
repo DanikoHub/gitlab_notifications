b879cdcf98392c1c7660ae8b7de1d0338e06cae2
Даниил Боровков <194267398+DanikoHub@users.noreply.github.com>
2025-02-11 11:32:08 +0300

    If log file does not exist, it will be created

--- mysite/src/log_tools.py
+++ mysite/src/log_tools.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 log_name = "mysite/src/logs/app_log.log"
 
 logging.basicConfig(
-    handlers = [RotatingFileHandler(log_name, maxBytes = 20000, backupCount = 0)],
+    handlers = [RotatingFileHandler(log_name, maxBytes = 2000, backupCount = 0, mode = "a+")],
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(message)s")
 
